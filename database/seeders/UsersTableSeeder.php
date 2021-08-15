e937f90cda7d2bee10ae98fe7bb3426cf1de2192
haniputraa <hanif.11.a2@gmail.com>
2021-08-15 11:31:55 +0700

    ganti nama userstableseeder.php

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Carbon;
 
-class UsersTableDatabase extends Seeder
+class UsersTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
