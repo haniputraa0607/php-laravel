d9f3bc527fd52abc5c4f9510f338cd682da8bd9b
haniputraa <hanif.11.a2@gmail.com>
2021-08-15 12:02:01 +0700

    membuat route baru

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ use App\Http\Controllers\HomeController;
 
 Auth::routes();
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::resource("companies", CompaniesController::class);
+Route::resource("employees", EmployeesController::class);
